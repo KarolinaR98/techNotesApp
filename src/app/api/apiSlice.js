e47369143276782ac817d332feed9874256b0998
KarolinaR98 <64532044+KarolinaR98@users.noreply.github.com>
2024-12-14 18:01:22 +0100

    api adress update

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://technotesapp-s0xi.onrender.com',
+    baseUrl: 'https://technotes-api-y4rs.onrender.com/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
